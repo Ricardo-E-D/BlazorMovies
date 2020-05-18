0a5a98b8c915efadb7f6f46fad0f0aa6500ef30c
Ricardo-E-D <47738496+Ricardo-E-D@users.noreply.github.com>
2020-05-18 17:53:10 +0200

    Partial Classes moving all code fromthe Counter page to its partial class

---
+++ BlazorMovies.BlazorWeb/Client/Pages/Counter.razor.cs
@@ -0,0 +1,35 @@
+﻿using BlazorMovies.BlazorWeb.Client.Services;
+using BlazorMovies.BlazorWeb.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.BlazorWeb.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject] SingletonService singleton { get; set; }
+        [Inject] TransientService transient { get; set; }
+
+        private List<Movie> movies;
+        private int currentCount = 0;
+
+        protected override void OnInitialized()
+        {
+            movies = new List<Movie>()
+    {
+            new Movie(){Title = "Joker",ReleaseDate = new DateTime(2019, 7, 2)},
+            new Movie(){Title = "Avengers",ReleaseDate = new DateTime(2016,11,23)}
+        };
+        }
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            singleton.Value = currentCount;
+            transient.Value = currentCount;
+        }
+    }
+}
